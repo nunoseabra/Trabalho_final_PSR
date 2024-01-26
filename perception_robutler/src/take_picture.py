211eb86c68326ca1dd0be0f1cb7184df63ef33da
Nuno Seabra <nunomsfs@ua.pt>
2024-01-26 13:49:19 +0000

    camera da picture

--- perception_robutler/src/take_picture.py
+++ perception_robutler/src/take_picture.py
@@ -32,7 +32,7 @@ class TakePhoto:
         self.save_path = save_path  # Adiciona o caminho da pasta
 
         # Connect image topic
-        img_topic = "/camera/rgb/image_raw"
+        img_topic = "/camera2/rgb/image_raw2"
         self.image_sub = rospy.Subscriber(img_topic, Image, self.callback)
 
         # Allow up to one second to connection
